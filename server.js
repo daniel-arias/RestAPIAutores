5b74bc188c9a5a7aef6d9d8e79629fb43c1f98e5
Leidy Sanchez <leysanchezb92@hotmail.com>
2020-08-20 20:15:38 -0500

    adding libros endpoint ... la leidy

--- server.js
+++ server.js
@@ -4,4 +4,61 @@ const server = express();
 
 server.listen(3000, () => {
     console.log('Start Server...');
+});
+
+const autores =[
+    {
+        id: 1,
+        nombre: 'Jorge Luis',
+        apellido: 'Borges',
+        fechaDeNacimiento: '24/08/1889',
+        libros: [
+            {
+                id:1,
+                titulo: 'ficciones',
+                descripcion: 'Se trata de uno de sus mas...',
+                anioPublicacion: 1944
+            },{
+                id:2,
+                titulo: 'El Aleph',
+                descripcion: 'Otra recopilacion de cuentos...',
+                anioPublicacion: 1949
+            },
+        ]
+    },{
+        id: 2,
+        nombre: 'Gabriel',
+        apellido: 'Garcia',
+        fechaDeNacimiento: '24/08/1889',
+        libros: [
+            {
+                id:1,
+                titulo: '100 años de soledad',
+                descripcion: 'Se trata de uno de sus mas...',
+                anioPublicacion: 1944
+            },{
+                id:2,
+                titulo: 'La mala hora',
+                descripcion: 'Otra recopilacion de cuentos...',
+                anioPublicacion: 1949
+            },
+        ]
+    },
+]
+
+
+
+server.get('/autores/:id/libros',(req,res)=>{
+    const libros = autores.find((item)=> {
+        if(item.id==req.params.id){
+            return item;
+        }
+    });
+
+    if(!libros){
+        res.status(404);
+        res.json({"message":"Autor no encontrado"});
+    } else{
+        res.json(libros.libros);
+    }
 })
