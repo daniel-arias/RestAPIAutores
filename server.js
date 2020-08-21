01f29f745b63b67d7aa9af6f212a24201673255c
daniel_arias <daniel.arias@skyline.be>
2020-08-20 20:00:53 -0500

    middlewares

--- server.js
+++ server.js
@@ -5,3 +5,29 @@ const server = express();
 server.listen(3000, () => {
     console.log('Start Server...');
 })
+
+const autororesDA = [
+    {
+
+    }
+]
+
+//middleware Autores
+function GetAutoresValidator(req, res, next)
+{
+
+}
+
+function PostAutoresValidator(req, res, next)
+{
+    const {body} = req;
+    if (!body) {
+        next(err);
+    }
+    autores.forEach(autor => {
+        if(autor.nombre === body.nombre && autor.apellido === body.apellido)
+        {
+            next(err);
+        }
+    });
+}
